78749c386c573d49c63136bec6b72eab60a78dd6
Steven Hall <stevenbryanb.work@gmail.com>
2024-03-12 21:03:45 +0000

    Added intitial loop test

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -10,19 +10,23 @@ namespace BetterDecorator
 
 
             // indexation
-
             string s = args[0];
 
             char c =  char.Parse(args[1]);
 
             int i = int.Parse(args[2]);
 
-
+            // calling Decor method
             Decor(s, c, i);
         }
 
         private static void Decor(string s , char dec, int i)
         {
+            // add in each side of the string the character dec i times
+            for (int x = 0; x < i; x++)
+            {
+                s = dec + s + dec;
+            }
 
 
         }
