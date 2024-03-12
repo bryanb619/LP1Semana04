7ba126c1b268db9f80a79ba3cd9a0ec0f657b10a
Steven Hall <stevenbryanb.work@gmail.com>
2024-03-12 20:58:25 +0000

    Added call to Decor method

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -8,15 +8,22 @@ namespace BetterDecorator
         {
             Console.WriteLine("Hello, World!");
 
+
+            // indexation
+
             string s = args[0];
 
             char c =  char.Parse(args[1]);
 
             int i = int.Parse(args[2]);
+
+
+            Decor(s, c, i);
         }
 
         private static void Decor(string s , char dec, int i)
         {
+
 
         }
     }
