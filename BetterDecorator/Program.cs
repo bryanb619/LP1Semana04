03dffa20986a59b8fed0510d9617f239a1f0c7f1
Steven Hall <stevenbryanb.work@gmail.com>
2024-03-12 20:56:41 +0000

    Added indexing to receive args

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -7,6 +7,17 @@ namespace BetterDecorator
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            string s = args[0];
+
+            char c =  char.Parse(args[1]);
+
+            int i = int.Parse(args[2]);
+        }
+
+        private static void Decor(string s , char dec, int i)
+        {
+
         }
     }
 }
