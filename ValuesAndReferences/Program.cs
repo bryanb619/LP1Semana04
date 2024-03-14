12bfd532e493d93756df49571dc6569e18c2b37b
Steven Hall <stevenbryanb.work@gmail.com>
2024-03-14 19:00:00 +0000

    Added comments to identify variables modification & logic

--- ValuesAndReferences/Program.cs
+++ ValuesAndReferences/Program.cs
@@ -8,24 +8,24 @@ namespace ValuesAndReferences
         {
             // Variables
             int x0 = 9;
-            int[] y0 = new int[] { 2, 3, 4 };
-            int x1 = x0;
-            int[] y1 = y0;
+            int[] y0 = new int[] { 2, 3, 4 }; // creates array ==> HEAP
+            int x1 = x0; // equals x1 to x0(9)
+            int[] y1 = y0; // creates ref to yo
 
             // Show x0 and y0 contents
             Console.WriteLine("Original contents:");
             ShowVars(x0, y0);
 
             // Change x1 and y1
-            x1 = 1;
-            y1[1] = 1;
+            x1 = 1; // only changes x1 to 1
+            y1[1] = 1; // =====> changes y0 and y1 [1] to 1
 
             // Check what happened to x0 and y0
             Console.WriteLine("After changing x1 and y1 values:");
             ShowVars(x0, y0);
 
             // Change y1 again and check what happens to y0
-            y1 = new int[] { -5, -4, -3 };
+            y1 = new int[] { -5, -4, -3 }; // ========> THIS DOES NOT CHANGE y0
             Console.WriteLine("After changing y1 reference:");
             ShowVars(x0, y0);
         }
