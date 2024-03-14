10ca3065d5e603ecb2fb59aac1a2efb7f762b555
Steven Hall <stevenbryanb.work@gmail.com>
2024-03-14 19:01:46 +0000

    Added better comments to Special

--- Special/Program.cs
+++ Special/Program.cs
@@ -13,35 +13,42 @@ namespace Special
             // convert  input to int
             int n = int.Parse(Console.ReadLine());
 
-            // request result from operation
+            // request result from operation => calls GetSpecial(n parameter)
             int result = GetSpecial(n);
 
             // print result
             Console.WriteLine(result);
 
-
         }
 
-        private static int GetSpecial(int n)
+        private static int GetSpecial(int n) 
         {
 
+
+            // temp variable that stores the result
             int calcResult; 
 
+            // return 0 
             if (n== 0)
             {
                 calcResult = 0;
             } 
 
+            // return 1
             else if(n == 1)
             {
                 calcResult = 1;
             }
 
+
             else
-            {
+            {         
+                // calculate special recursively
                 calcResult = GetSpecial(n-1) + 2 * GetSpecial(n-2);
+             
             }
         
+            // return calculation to caller
             return calcResult;
         }
     }
